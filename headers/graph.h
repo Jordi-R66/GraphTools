6574cfba2f39828916334f5a9fb4fe594a17433b
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-01 18:23:22 +0200

    Ajout de commentaires pour clarifier les propriétés de la structure Graph

--- headers/graph.h
+++ headers/graph.h
@@ -5,6 +5,11 @@
 typedef struct Graph {
 	List Vertices;
 	List Edges;
+
+	// From Right to Left
+	// Bit 1 : Directed
+	// Bit 2 : Weighted
+	bool properties : 2;
 } Graph;
 
 #define GRAPH_SIZE sizeof(Graph);
