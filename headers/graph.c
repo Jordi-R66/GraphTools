2c0eb27d5e8850fdb82a7c2445398396e6fc4ec0
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-02 02:20:30 +0200

    Ajout de la fonction initEdges pour initialiser les arêtes d'un graphe

--- headers/graph.c
+++ headers/graph.c
@@ -7,3 +7,7 @@ void initGraph(Graph* graph) {
 void initVertices(Graph* graph, gsize_t maxOrder) {
 	initializeList(&graph->Vertices, maxOrder, VERTEX_SIZE);
 }
+
+void initEdges(Graph* graph, gsize_t maxSize) {
+	initializeList(&graph->Edges, maxSize, EDGE_SIZE);
+}
