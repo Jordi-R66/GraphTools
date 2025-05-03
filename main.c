0c81091d0dafea6b6d2c641d2eddb123f1d9be62
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-03 19:13:51 +0200

    Correction de l'initialisation de la taille du graphe dans main

--- main.c
+++ main.c
@@ -5,11 +5,10 @@
 void main() {
 	Graph graph;
 
-	initFullGraph(&graph, 128, false, false);
+	initFullGraph(&graph, 5, false, false);
 	fillGraph(&graph);
 
 	List E = graph.Edges;
-	List X = graph.Vertices;
 
 	printf("M = %lu\n", E.capacity);
 	deallocGraph(&graph);
