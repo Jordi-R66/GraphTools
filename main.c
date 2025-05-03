f573b6c8f0e8e2cb3d75e6eed4cff4f098968c52
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-03 20:18:48 +0200

    Mise à jour de l'initialisation du graphe pour utiliser une capacité de 1024 et ajout d'une déclaration de variable pour la lecture des entrées

--- main.c
+++ main.c
@@ -5,11 +5,16 @@
 void main() {
 	Graph graph;
 
-	initFullGraph(&graph, 256, false, false);
+	initFullGraph(&graph, 1024, false, false);
 	fillGraph(&graph);
 
 	List E = graph.Edges;
 
 	printf("M = %lu\n", E.capacity);
+	char c;
+
+	while (c != 'E') {
+		scanf("%c", &c);
+	}
 	deallocGraph(&graph);
 }
