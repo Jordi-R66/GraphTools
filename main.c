7a0111c4f816c497811118974ba76c648344b907
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-03 20:23:19 +0200

    Mise à jour de l'initialisation du graphe pour utiliser une capacité de 256 et suppression de la boucle de lecture des entrées

--- main.c
+++ main.c
@@ -5,16 +5,12 @@
 void main() {
 	Graph graph;
 
-	initFullGraph(&graph, 1024, false, false);
+	initFullGraph(&graph, 256, false, false);
 	fillGraph(&graph);
 
 	List E = graph.Edges;
 
 	printf("M = %lu\n", E.capacity);
-	char c;
 
-	while (c != 'E') {
-		scanf("%c", &c);
-	}
 	deallocGraph(&graph);
 }
