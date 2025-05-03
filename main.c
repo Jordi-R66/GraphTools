c80b41ff3b38592270c6630b212cb76f6c300c5b
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-03 18:03:28 +0200

    Ajout de l'initialisation et de l'affichage des graphes dans la fonction main

--- main.c
+++ main.c
@@ -3,4 +3,16 @@
 #include <stdio.h>
 
 void main() {
+	Graph graph;
+
+	initFullGraph(&graph, 5, false, false);
+
+	List E = graph.Edges;
+	List X = graph.Vertices;
+
+	printf("M = %lu\n", E.capacity);
+	fillGraph(&graph);
+
+	printf("n = %lu\nm = %lu\n", X.n_elements, E.n_elements);
+	deallocGraph(&graph);
 }
