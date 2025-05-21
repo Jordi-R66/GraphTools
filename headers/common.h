5ec4cd22d7433f78c5e9b8ff694352b4997a0287
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-21 11:54:29 +0200

    Mise à jour de l'inclusion des macros dans common.h et mise à jour du sous-projet myOwnCLib vers le commit bf9bead

--- headers/common.h
+++ headers/common.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../libs/myOwnCLib/targetDetection/macros.h"
+#include "../libs/myOwnCLib/targetSpecific/macros.h"
 
 #include "../libs/myOwnCLib/lists/list.h"
 #include "../libs/myOwnCLib/bits/bits.h"
