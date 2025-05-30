9f29920fa2936763866318d854c64b4ea806d8fb
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-30 13:10:10 +0200

    Réorganisation des inclusions dans common.h et ajout de la définition de la matrice dans representations.h, mise à jour du sous-projet myOwnCLib

--- headers/common.h
+++ headers/common.h
@@ -2,7 +2,6 @@
 
 #include "../libs/myOwnCLib/targetSpecific/macros.h"
 
-#include "../libs/myOwnCLib/matrices/matrix.h"
 #include "../libs/myOwnCLib/lists/list.h"
 #include "../libs/myOwnCLib/bits/bits.h"
 
--- headers/representations.h
+++ headers/representations.h
@@ -2,4 +2,9 @@
 
 #include "graph.h"
 
+#define OVERRIDE_MATRIX_VALUE_TYPE
+#define VALUE_TYPE gorder_t
+
+#include "../libs/myOwnCLib/matrices/matrix.h"
+
 Matrix adjacencyMatrix(Graph* graph);
